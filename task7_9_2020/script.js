4a48342370fe60142af24eedfbe14d3e911dc97e
Saba-Hatamleh <sabsoobryad@outlook.com>
2020-09-07 12:01:18 +0300

    ..

---
+++ task7_9_2020/script.js
@@ -0,0 +1,23 @@
+alert("you are in home");
+
+
+function fun1()
+{
+    document.write("Hi, you are in Home page");
+}
+
+function fun2()
+{
+    document.getElementById("xid").innerHTML="Saba Hatamleh";
+}
+
+
+function myFunction(x1, x2) {
+    return x1 * x2;
+ 
+
+  }
+
+  
+ var x=" world"; 
+    console.log('Hello'+x);
